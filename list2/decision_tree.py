3f020582bbe376dfe05eb4989e01c47853d2b1e8
NeodymiumOre <maciejbarna@gmail.com>
2023-11-21 15:08:22 +0100

    Finished code for decision tree

--- list2/decision_tree.py
+++ list2/decision_tree.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
 
 
 def import_data_from_csv(file):
@@ -11,7 +14,7 @@ def import_data_from_csv(file):
     data.columns = ['feature 1', 'feature 2', 'label']
     return data
 
-def colorplot_2d_data(data):
+def colorplot_2d_data(data, colour):
     """Plot array of data on 2D colorplot"""
 
     # divide data into 2 groups based on label value
@@ -24,22 +27,19 @@ def colorplot_2d_data(data):
     plt.scatter(feature1['feature 1'].to_numpy(),
                 feature1['feature 2'].to_numpy(),
                 s=marker_size,
-                marker='o',
-                c='b',
+                marker='*',
+                c=colour,
                 label='0')
     plt.scatter(feature2['feature 1'].to_numpy(),
                 feature2['feature 2'].to_numpy(),
                 s=marker_size,
                 marker='o',
                 facecolors='none',
-                edgecolors='k',
+                edgecolors=colour,
                 label='1')
     plt.xlabel('feature 1')
     plt.ylabel('feature 2')
-    plt.title("Visualized dataset from CSV")
-    plt.savefig("pictures/tree/xd.png")
-
-    plt.show()
+    # plt.title("Visualized dataset from CSV")
 
 
 if __name__ == "__main__":
@@ -50,4 +50,54 @@ if __name__ == "__main__":
     file = "data.csv"
     data = import_data_from_csv(file)
 
-    colorplot_2d_data(data)
+    # plot imported data and save the image
+    colorplot_2d_data(data, 'b')
+    plt.savefig('pictures/tree/oryginal_dataset.png')
+    plt.show()
+
+    # split dataset into train and test
+    x_train, x_test, y_train, y_test = train_test_split(data[['feature 1', 'feature 2']].to_numpy(),
+                                                        data['label'].to_numpy(),
+                                                        random_state=1,
+                                                        test_size=0.2)
+    train_data = pd.DataFrame({'feature 1':x_train.T[0], 'feature 2':x_train.T[1], 'label':y_train})
+
+    # PERFORM CLASSIFICATION WITH DEFAULT HYPERPARAMETERS
+
+    tree_model = DecisionTreeClassifier()
+    tree_model.fit(x_train, y_train)
+    tree_predictions = tree_model.predict(x_test)
+    score = tree_model.score(x_test, y_test)
+    print(f"Accuracy score of decision tree with default hyperparameters: {score:.4}")
+    print("Default hyperparameters:")
+    print(f"[criterion: {tree_model.criterion}, depth: {tree_model.get_depth()}, min_samples_leaf: {tree_model.min_samples_leaf}]")
+
+    # EXPERIMENT WITH HYPERPARAMETERS
+
+    # choose hyperparameters for test with their values
+    criterion = ("gini", "entropy", "log_loss")
+    min_samples_leaf = range(1, 20, 1)
+    max_depth = range(3, 20, 1)
+
+    parameters = dict(criterion=criterion, max_depth=max_depth, min_samples_leaf=min_samples_leaf)
+
+    # create model for grid searching
+    gs_model = GridSearchCV(DecisionTreeClassifier(random_state=1), parameters, n_jobs=-1)
+    gs_model.fit(data[['feature 1', 'feature 2']].to_numpy(), data['label'].to_numpy())
+
+    # prnit best result
+    print("Best hyperparameters' values from grid search:")
+    print(f"{str(gs_model.best_estimator_)}, {gs_model.best_score_:.3}")
+
+    # PLOT RESULTS
+
+    # create subsets of data
+    data_train = pd.DataFrame({'feature 1': x_train.T[0], 'feature 2': x_train.T[1], 'label': y_train})
+    data_test = pd.DataFrame({'feature 1': x_test.T[0], 'feature 2': x_test.T[1], 'label': y_test})
+
+    # plot results
+    colorplot_2d_data(data_train, 'k')
+    colorplot_2d_data(data_test, 'b')
+    plt.savefig('pictures/tree/fitted_dataset.png')
+    plt.show()
+
